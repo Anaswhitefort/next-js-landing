ede0bc8decd91462f6dd0338dc73bd2d74bf983d
Mathieu <mathieu.tanguay871@gmail.com>
2023-06-13 23:03:09 -0400

    Remove leaked credentials

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,8 +4,8 @@ import GithubProvider from 'next-auth/providers/github'
 const handler = NextAuth({
   providers: [
     GithubProvider({
-      clientId: "9319eb2a5af24db9b59b",
-      clientSecret: "51aa491a90c32c08cc1995291f72ae250a53f103"
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_SECRET!
     })
   ] 
 });
